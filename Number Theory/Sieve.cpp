f8e4f2aa2a9aa7f9f827749271d9dc96ae9bdd0d
Tushar Sarkar <56844241+TusharCSE35@users.noreply.github.com>
2021-06-15 20:27:05 +0600

    Create Sieve.cpp

---
+++ Number Theory/Sieve.cpp
@@ -0,0 +1,100 @@
+/*********************************************************************************\
+*  _________  _      _  ________  _       _       __       ________               *
+* |___   ___|| |    | ||  ______|| |     | |     /  \     |  ____  |              *
+*     | |    | |    | || |______ | |_____| |    / /\ \    | |____| |              *
+*     | |    | |    | ||______  ||  _____  |   / /__\ \   |  __  __|              *
+*     | |    | |____| | ______| || |     | |  / ______ \  | |  \ \                *
+*     |_|    |________||________||_|     |_| /_/      \_\ |_|   \_\               *
+*                                                                                 *
+*   Department of Computer Science & Engineering                                  *
+*   Student ID : 18CSE035                                                         *
+*   Bangabnadhu Sheikh Mujibur Rahman Science & Technology University,Gopalgonj.  *
+*                                                                                 *
+\*********************************************************************************/
+
+//Now Write to Code ___________________________
+
+
+#include <bits/stdc++.h>
+using namespace std ;
+ 
+ 
+typedef long long                ll;
+typedef long double              ld;
+typedef unsigned long long       ull;
+typedef pair <int,int>           pii;
+typedef pair <ll,ll>             pll;
+typedef vector <int>             vi;
+typedef vector <ll>              vll;
+typedef vector <vector <int> >   vvi;
+ 
+inline int    Int(){int x; cin >> x; return x;}
+inline ll     Long(){ll x; cin >> x; return x;}
+inline float  Float(){float x; cin >> x; return x;}
+inline double Double(){double x; cin >> x; return x;}
+inline void   Yes(){cout << "Yes" << endl;}
+inline void   No(){cout << "No" << endl;}
+inline void   YES(){cout << "YES" << endl;}
+inline void   NO(){cout << "NO" << endl;}
+ 
+const int N             =(int)2e5 + 5;
+const int maxN          =(int)1e6 + 6;
+const ll  Mod           =(ll)1e9 + 7;
+const int inf           =(int)2e9;
+const ll  Inf           =(ll)1e18;
+ 
+#define     T           int t,q; cin >> t; for(q=1;q<=t;q++)
+#define     debug(x)    cerr << #x << " = " << x << '\n' ;
+#define     rep(i,b,e)  for(__typeof(e) i=(b); i!=(e+1)-2*(b > e); i =i+1-2*(b > e))
+#define     Int         Int()
+#define     Long        Long()
+#define     Float       Float()
+#define     Double      Double()
+#define     all(x)      x.begin() , x.end()
+#define     sz(x)       (int)x.size()
+#define     ff          first
+#define     ss          second
+#define     pb          push_back
+#define     eb          emplace_back
+#define     mem(a)      memset(a , 0 ,sizeof a)
+#define     memn(a)     memset(a , -1 ,sizeof a)
+#define     biday       return 0 
+#define     PI          3.141592653589793238462643383279502884L
+
+
+ 
+int main()
+{
+     T{
+	    ll n,i,j,limit,mark[1000005]={0},cnt=0;
+	    vll v;
+            cin >> n;
+
+            mark[1]=1;
+	    v.pb(2);
+	    limit = sqrt(n+1);
+	    for(i = 4; i <= n; i+=2)mark[i]=1;
+	    for(i = 3; i <= n; i+=2)
+	    {
+		    if(!mark[i])
+		    {
+			    v.pb(i);
+			    if(i <= limit)
+			    {
+				    for(j = i*i; j <= n; j+=i*2)
+				    {
+					    mark[j]=1;
+				    }
+			    }
+		    }
+	    }
+
+	    for(i=1; i<v.size()-1; i++)if(v[i+1]-v[i]==2)cnt++;
+	    cout << cnt << endl;
+     }
+
+
+     biday;
+}
+ 
+//...............BYE BYE................
