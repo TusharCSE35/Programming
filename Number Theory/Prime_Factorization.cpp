280ad83dae3416c3f2a682725b5faaea8e7141b0
Tushar Sarkar <56844241+TusharCSE35@users.noreply.github.com>
2021-06-15 20:28:19 +0600

    Create Prime_Factorization.cpp

---
+++ Number Theory/Prime_Factorization.cpp
@@ -0,0 +1,134 @@
+/*********************************************************************************\
+*  _________  _      _  ________  _       _       __       ________               *
+* |___   ___|| |    | ||  ______|| |     | |     /  \     |  ____  |              *
+*     | |    | |    | || |______ | |_____| |    / /\ \    | |____| |              *
+*     | |    | |    | ||______  ||  _____  |   / /__\ \   |  __  __|              *
+*     | |    | |____| | ______| || |     | |  / ______ \  | |  \ \                *
+*     |_|    |________||________||_|     |_| /_/      \_\ |_|   \_\               *
+*                                                                                 *
+*   Department of Computer Science & Engineering                                  *
+*   Student ID : 18CSE035                                                         *
+*   Bangabnadhu Sheikh Mujibur Rahman Science & Technology University,Gopalgonj.  *
+*                                                                                 *
+\*********************************************************************************/
+
+//Now Write to Code ___________________________
+
+
+#include <bits/stdc++.h>
+using namespace std ;
+ 
+ 
+typedef    long long             ll;
+typedef    long double           ld;
+typedef    unsigned long long    ull;
+typedef    pair<int,int>         pii;
+typedef    pair<ll,ll>           pll;
+typedef    vector<int>           vi;
+typedef    vector<ll>            vll;
+typedef    vector<vector<int>>   vvi;
+ 
+inline     int         Int(){int x; cin >> x; return x;}
+inline     ll          Long(){ll x; cin >> x; return x;}
+inline     float       Float(){float x; cin >> x; return x;}
+inline     double      Double(){double x; cin >> x; return x;}
+inline     void        Yes(){cout << "Yes" << endl;}
+inline     void        No(){cout << "No" << endl;}
+inline     void        YES(){cout << "YES" << endl;}
+inline     void        NO(){cout << "NO" << endl;}
+ 
+const      int N       =(int)2e5 + 5;
+const      int maxN    =(int)1e6 + 6;
+const      ll  Mod     =(ll)1e9 + 7;
+const      int inf     =(int)2e9;
+const      ll  Inf     =(ll)1e18;
+
+#define    fastio      ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define    T           int t,q; cin >> t; for(q=1;q<=t;q++)
+#define    Forn(i,n)   for(int i=0;i<n;i++)
+#define    ForN(i,n)   int i;for(i=n-1;n>=0;i--)
+#define    forn(i,n)   for(int i=1;i<=n;i++)
+#define    forN(i,n)   int i;for(i=n;n>=1;i--)
+#define    fors(i,s)   for(int i=0;i<s.size();i++)
+#define    Sort(s)     sort(s.begin(),s.end())
+#define    debug(x)    cerr << #x << " = " << x << '\n' ;
+#define    rep(i,b,e)  for(__typeof(e) i=(b); i!=(e+1)-2*(b>e); i=i+1-2*(b>e))
+#define    gcd(a,b)    __gcd(a , b)
+#define    lcm(a,b)    (a*(b/__gcd(a,b)))
+#define    gt          greater<int>()
+#define    Int         Int()
+#define    Long        Long()
+#define    Float       Float()
+#define    Double      Double()
+#define    all(x)      x.begin() , x.end()
+#define    sz(x)       (int)x.size()
+#define    ff          first
+#define    ss          second
+#define    pb          push_back
+#define    eb          emplace_back
+#define    mp          make_pair
+#define    mem(a)      memset(a,0,sizeof a)
+#define    memn(a)     memset(a,-1,sizeof a)
+#define    biday       return 0 
+#define    nl          endl
+#define    sp(n)       fixed<<setprecision(n)
+#define    pi          acos(-1)
+#define    PI          3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342
+
+int mark[100000000]; 
+vll sv,pfv;
+
+void prime_factor(int n)
+{
+	   int i,lenth=sqrt(n);
+	   for(i=0; sv[i]<=lenth; i++)
+	   {
+		       while(n%sv[i]==0)
+		       {
+			          pfv.pb(sv[i]);
+			          n/=sv[i];
+		       }
+	    }
+	    if(n>1)pfv.pb(n);
+}
+void sieve(int n)
+{
+	    int i,j,limit=sqrt(n);
+
+	    if(n>=1)mark[1]=1;
+	    if(n>=2)sv.pb(2);
+	    for(i=4; i<=n; i+=2)mark[i]=1;
+	    for(i=3; i<=n; i+=2)
+	    {
+		        if(!mark[i])
+		        {
+			           sv.pb(i);
+			           if(i<=limit) for(j=i*i; j<=n; j+=i*2) mark[j]=1;
+		        }
+	    }	
+}
+ 
+int main()
+{
+	      cout << "Enter test case : ";
+        T{
+   	     int n,i;
+             cout << "Plz enter the value of N : ";
+             cin >> n;
+
+             sieve(n);
+             cout << "Prime numbers of N : ";
+             for(i=0; i<sv.size(); i++)cout << sv[i] << " ";
+             cout << endl;
+
+             prime_factor(n);
+             cout << "Prime Factor of N : ";
+             for(i=0; i<pfv.size(); i++)cout << pfv[i] << " ";
+             cout << endl;	
+   }
+    
+   
+    biday;
+}
+ 
+//...............BYE BYE................
