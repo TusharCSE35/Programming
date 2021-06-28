4231b23cbaa1e41c939827f6bc37b92e03d8494f
Tushar Sarkar <56844241+TusharCSE35@users.noreply.github.com>
2021-06-28 23:21:34 +0600

    Create Floyd_Warshal.cpp

---
+++ Graph/Floyd_Warshal.cpp
@@ -0,0 +1,144 @@
+/*********************************************************************************\
+*  _________  _      _  ________  _       _       __       ________               *
+* |___   ___|| |    | ||  ______|| |     | |     /  \     |  ____  |              *
+*     | |    | |    | || |______ | |_____| |    / /\ \    | |____| |              *
+*     | |    | |    | ||______  ||  _____  |   / /__\ \   |  __  __|              *
+*     | |    | |____| | ______| || |     | |  / ______ \  | |  \ \                *
+*     |_|    |________||________||_|     |_| /_/      \_\ |_|   \_\               *
+*                                                                                 *
+*   Department of Computer Science & Engineering                                  *
+*   Student ID : 18CSE035                                                         *
+*   Bangabnadhu Sheikh Mujibur Rahman Science & Technology University,Gopalgonj.  *
+*                                                                                 *
+\*********************************************************************************/
+
+//Now Write to Code ___________________________
+
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+
+typedef    long long             ll;
+typedef    unsigned long long    ull;
+typedef    vector<ll>            vl;
+typedef    vector<int>           vi;
+typedef    vector<char>          vc;
+typedef    vector<string>        vs;
+typedef    vector<int>::iterator vit;
+typedef    set<int>              si;
+typedef    set<string>           ss;
+typedef    set<int>::iterator    sit;
+typedef    map<int, int>         mii;
+typedef    map<string, int>      msi;
+typedef    map<int, string>      mis;
+typedef    pair<ll, ll>          pll;
+typedef    pair<int, int>         pii;
+
+inline     int         Int()     {int x; cin >> x; return x;}
+inline     ll          Long()    {ll x; cin >> x; return x;}
+inline     float       Float()   {float x; cin >> x; return x;}
+inline     double      Double()  {double x; cin >> x; return x;}
+inline     void        Yes()     {cout << "Yes" << endl;}
+inline     void        No()      {cout << "No" << endl;}
+inline     void        YES()     {cout << "YES" << endl;}
+inline     void        NO()      {cout << "NO" << endl;}
+
+const      int N       = (int)2e5 + 5;
+const      int maxN    = (int)1e6 + 6;
+const      ll  Mod     = (ll)1e9 + 7;
+const      int inf     = (int)2e9;
+const      ll  Inf     = (ll)1e18;
+
+#define    T           int t,q; cin >> t; for(q=1;q<=t;q++)
+#define    Forn(i,n)   for(int i=0;i<n;i++)
+#define    ForN(i,n)   for(int i=n-1;i>=0;i--)
+#define    forn(i,n)   for(int i=1;i<=n;i++)
+#define    forN(i,n)   for(int i=n;i>=1;i--)
+#define    fors(i,s)   for(int i=0;i<s.size();i++)
+#define    Sort(s)     sort(s.begin(),s.end())
+#define    debug(x)    cerr << #x << " = " << x << '\n' ;
+#define    rep(i,b,e)  for(__typeof(e) i=(b); i!=(e+1)-2*(b>e); i=i+1-2*(b>e))
+#define    gcd(a,b)    __gcd(a , b)
+#define    lcm(a,b)    (a*(b/__gcd(a,b)))
+#define    all(a)      (a).begin(),(a).end()
+#define    rall(a)     (a).rbegin(),(a).rend()
+#define    gt          greater<int>()
+#define    Int         Int()
+#define    Long        Long()
+#define    Float       Float()
+#define    Double      Double()
+#define    endl        '\n'
+#define    sp          <<" "<<
+#define    sz(x)       (int)x.size()
+#define    f           first
+#define    s           second
+#define    pb          push_back
+#define    eb          emplace_back
+#define    mp          make_pair
+#define    mem(a)      memset(a,0,sizeof a)
+#define    memn(a)     memset(a,-1,sizeof a)
+#define    biday       return 0
+#define    nl          endl
+#define    fstp(n)       fixed<<setprecision(n)
+#define    pi          acos(-1)
+#define    PI          3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342
+
+
+#define L 10005
+ll dis[L][L];
+
+void percel(ll n) {
+    ll i, j;
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            if (i == j)dis[i][j] = 0;
+            else dis[i][j] = 50;
+        }
+    }
+
+}
+
+void FloydWarshal(ll n) {
+    ll i, j, k;
+
+    for (k = 1; k <= n; k++) {
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n; j++) {
+                if (dis[i][k] + dis[k][j] < dis[i][j]) {
+                    dis[i][j] = dis[i][k] + dis[k][j];
+                }
+            }
+        }
+    }
+}
+
+int main() {
+
+    ll node, edge, i, j, q;
+    cin >> node >> edge;
+
+    percel(node);
+
+    while (edge--) {
+        ll u, v, w;
+        cin >> u >> v >> w;
+
+        dis[u][v] = w;
+    }
+
+    FloydWarshal(node);
+
+    cin >> q;
+    while (q--) {
+        ll x, y;
+        cin >> x >> y;
+        if (dis[x][y] == Inf)cout << "Infinity" << endl;
+        else cout << "Distance from " << x << " to " << y << " is : " << dis[x][y] << endl;
+    }
+
+
+    biday;
+}
+
+//...............BYE BYE................
