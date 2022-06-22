55db44ebe7747002e2d0599a2d0c090b861ebe75
Tushar Sarkar <56844241+TusharCSE35@users.noreply.github.com>
2022-06-22 19:31:21 +0600

    Create Segment_Tree_Lazy_Propagation.cpp

---
+++ Tree/Segment_Tree_Lazy_Propagation.cpp
@@ -0,0 +1,203 @@
+/*********************************************************************************\
+*  _________  _      _  ________  _       _       __       ________               *
+* |___   ___|| |    | ||  ______|| |     | |     /  \     |  ____  |              *
+*     | |    | |    | || |______ | |_____| |    / /\ \    | |____| |              *
+*     | |    | |    | ||______  ||  _____  |   / /__\ \   |  __  __|              *
+*     | |    | |____| | ______| || |     | |  / ______ \  | |  \ \                *
+*     |_|    |________||________||_|     |_| /_/      \_\ |_|   \_\               *
+*                                                                                 *
+*   Department of Computer Science & Engineering                                  *
+*   Student ID : 18CSE035                                                         *
+*   Bangabnadhu Sheikh Mujibur Rahman Science & Technology University,Gopalganj.  *
+*                                                                                 *
+\*********************************************************************************/
+
+//Now Write to Code ___________________________
+
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+
+typedef    long long             ll;
+typedef    unsigned long long    ull;
+typedef    vector<int>           vi;
+typedef    vector<ll>            vl;
+typedef    vector<char>          vc;
+typedef    vector<string>        vs;
+typedef    vector<int>::iterator vit;
+typedef    set<int>              si;
+typedef    set<string>           s_s;
+typedef    set<int>::iterator    sit;
+typedef    map<int, int>         mii;
+typedef    map<ll, ll>           mll;
+typedef    map<string, int>      msi;
+typedef    map<string, ll>       msl;
+typedef    map<int, string>      mis;
+typedef    map<int, string>      mls;
+typedef    pair<ll, ll>          pll;
+typedef    pair<int, int>        pii;
+typedef    pair<ll, ll>          pll;
+
+inline     int         Int()     {int x; cin >> x; return x;}
+inline     ll          Long()    {ll x; cin >> x; return x;}
+inline     float       Float()   {float x; cin >> x; return x;}
+inline     double      Double()  {double x; cin >> x; return x;}
+inline     void        Yes()     {cout << "Yes" << endl;}
+inline     void        No()      {cout << "No" << endl;}
+inline     void        YES()     {cout << "YES" << endl;}
+inline     void        NO()      {cout << "NO" << endl;}
+
+const      int N       = (int)2e5 + 5;
+const      int maxN    = (int)1e6 + 6;
+const      ll  Mod     = (ll)1e9 + 7;
+const      int inf     = (int)2e9;
+const      ll  Inf     = (ll)1e18;
+
+#define    test        int t,q; cin >> t; for(q=1;q<=t;q++)
+#define    For(i,p,n)  for(int i=p;i<=n;i++)
+#define    Forb(i,n,p) for(int i=n;i>=p;i--)
+#define    Fors(i,s)   for(int i=0;i<s.size();i++)
+#define    Sort(s)     sort(s.begin(),s.end())
+#define    debug(x)    cerr << #x << " = " << x << '\n' ;
+#define    rep(i,b,e)  for(__typeof(e) i=(b); i!=(e+1)-2*(b>e); i=i+1-2*(b>e))
+#define    lcm(a,b)    (a*b)/__gcd(a,b)
+#define    all(a)      (a).begin(),(a).end()
+#define    rall(a)     (a).rbegin(),(a).rend()
+#define    gt          greater<int>()
+#define    Long        Long()
+#define    Float       Float()
+#define    Double      Double()
+#define    endl        '\n'
+#define    sp          <<" "<<
+#define    sz(x)       (int)x.size()
+#define    ff          first
+#define    ss          second
+#define    pb          push_back
+#define    eb          emplace_back
+#define    mp          make_pair
+#define    bitcount(x) (int)__builtin_popcount(x)
+#define    mem(a)      memset(a,0,sizeof a)
+#define    memn(a)     memset(a,-1,sizeof a)
+#define    biday       return 0
+#define    nl          cout << endl
+#define    pt(s)       cout << s << " "
+#define    fstp(n)     fixed<<setprecision(n)
+#define    pi          acos(-1)
+
+//Error Debug
+#define    error(args...) {vector<string>_v=split(#args,',');err(_v.begin(),args);cout<<endl;}
+vector<string> split(const string &s, char c) {vector<string>v; stringstream ss(s); string x; while (getline(ss, x, c))v.emplace_back(x); return move(v);} void err(vector<string>::iterator it) {}
+template<typename T, typename... Args>void err(vector<string>::iterator it, T a, Args...args) {cout << it->substr((*it)[0] == ' ', it->length()) << " = " << a << " "; err(++it, args...);}
+
+//Input Int
+template <typename T> inline void Int(T &n) {
+	n = 0; int f = 1; register int ch = getchar();
+	for (; !isdigit(ch); ch = getchar()) if (ch == '-') f = -1;
+	for (; isdigit(ch); ch = getchar()) n = (n << 3) + (n << 1) + ch - '0';
+	n = n * f;
+}
+template <typename T, typename TT> inline void Int(T &n, TT &m) { Int(n); Int(m); }
+template <typename T, typename TT, typename TTT> inline void Int(T &n, TT &m, TTT &l) { Int(n, m); Int(l); }
+
+int dx[8] = {1, 0, -1, 0, -1, -1, 1, 1};
+int dy[8] = {0, 1, 0, -1, -1, 1, -1, 1};
+ll gcd(ll a, ll b) { if (b == 0) return a; return gcd(b, a % b);}
+ll ceil_div(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
+ll Inv_pow(ll a, ll n) {ll res = 1; while (n) {if (n & 1) res = ((res % Mod) * (a % Mod)) % Mod; a = ((a % Mod) * (a % Mod)) % Mod; n >>= 1;} return res % Mod;}
+
+#define Mx 10005
+ll arr[Mx];
+struct info {
+	ll prop, sum;
+} tree[Mx * 4];
+
+void init(ll node, ll b, ll e) {
+	ll left, right, mid;
+	if (b == e) {
+		tree[node].sum = arr[b];
+		return;
+	}
+
+	left = node * 2;
+	right = node * 2 + 1;
+	mid = (b + e) / 2;
+	init(left, b, mid);
+	init(right, mid + 1, e);
+	tree[node].sum = tree[left].sum + tree[right].sum;
+}
+
+void update(ll node, ll b, ll e, ll L, ll R, ll val) {
+	ll left, right, mid;
+	if (e < L or R < b)return;
+	if (L <= b and e <= R) {
+		tree[node].sum += (e - b + 1) * val;
+		tree[node].prop += val;
+		return;
+	}
+
+	left = node * 2;
+	right = node * 2 + 1;
+	mid = (b + e) / 2;
+	update(left, b, mid, L, R, val);
+	update(right, mid + 1, e, L, R, val);
+	tree[node].sum = tree[left].sum + tree[right].sum;
+}
+
+int query(ll node, ll b, ll e, ll L, ll R, ll carry = 0) {
+	ll left, right, mid, p1, p2;
+	if (e < L or R < b)return 0;
+	if (L <= b and e <= R)return tree[node].sum + carry * (e - b + 1);
+
+	left = node * 2;
+	right = node * 2 + 1;
+	mid = (b + e) / 2;
+	p1 = query(left, b, mid, L, R, carry + tree[node].prop);
+	p2 = query(right, mid + 1, e, L, R, carry + tree[node].prop);
+
+	return p1 + p2;
+}
+
+int solve() {
+	ll n, q;
+	cout << "Enter n and q: ";
+	cin >> n >> q;
+
+	cout << "Enter the element of n : ";
+	For(i, 1, n)cin >> arr[i];
+
+	init(1, 1, n);
+	cout << "Enter query : " << endl;
+	while (q--) {
+		ll x, a, b, c;
+		cout << "Enter the type : ";
+		cin >> x;
+		if (x == 1) {
+			cout << "Update range and value : ";
+			cin >> a >> b >> c;
+			update(1, 1, n, a, b, c);
+		}
+		else {
+			cout << "Enter range for query : ";
+			cin >> a >> b;
+			ll sum = query(1, 1, n, a, b);
+			cout << "Sum : " << sum << endl;
+		}
+	}
+
+
+	biday;
+}
+
+int main() {
+	int t = 1;
+	//cin >> t;
+	for (int i = 1; i <= t; i++) {
+		//error(i)
+		solve();
+	}
+
+	biday;
+}
+
+//...............BYE BYE................//
